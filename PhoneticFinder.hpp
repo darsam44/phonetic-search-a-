7d60625d9811918c19d5ab45136e8f44c88d70e0
Shani <shanishalel89@gmail.com>
2020-07-23 19:07:30 +0300

    pass all the ex for test

--- PhoneticFinder.hpp
+++ PhoneticFinder.hpp
@@ -4,6 +4,6 @@
 using namespace std;
 
 namespace phonetic {
-	/*find the words that were changes and return them with the mistakes*/
+	/*find the words that were changes and return them with the mistakes */
 	string find(string text, string word);
 }
